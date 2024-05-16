86313a621785243a77cb400176be66cde7065588
MikeEm <michalbanaszek03@gmail.com>
2024-05-16 15:51:22 +0200

    testing a sum script

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function oblicz() {
+    var l1 = document.getElementById('l1');
+    l1 = l1.value;
+
+    l1 = parseInt(l1);
+
+    var l2 = parseInt(document.getElementById('l2').value);
+
+    var s = document.getElementById('suma');
+    s.value = l1 + l2;
+}
+
