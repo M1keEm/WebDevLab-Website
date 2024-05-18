85c38faf0cd8cef9179d3fc60a1c420eb672a7b7
MikeEm <michalbanaszek03@gmail.com>
2024-05-18 20:27:03 +0200

    walidacja.js - working age

--- JS/walidacja.js
+++ JS/walidacja.js
@@ -1,51 +1,52 @@
 function sprawdzPole(pole_id, obiektRegex) {
-//Funkcja sprawdza czy wartość wprowadzona do pola tekstowego
-//pasuje do wzorca zdefiniowanego za pomocą wyrażenia regularnego
-//Parametry funkcji:
-//pole_id - id sprawdzanego pola tekstowego
-//obiektRegex - wyrażenie regularne
-//---------------------------------
     var obiektPole = document.getElementById(pole_id);
     return obiektRegex.test(obiektPole.value);
 }
 
 function sprawdz_radio(nazwa_radio) {
-//Funkcja sprawdza czy wybrano przycisk radio
-//z grupy przycisków o nazwie nazwa_radio
-//---------------------------------------
     var obiekt = document.getElementsByName(nazwa_radio);
-    for (i = 0; i < obiekt.length; i++) {
-        wybrany = obiekt[i].checked;
-        if (wybrany) return true;
+    for (let i = 0; i < obiekt.length; i++) {
+        if (obiekt[i].checked) return true;
     }
     return false;
 }
 
-function sprawdz_box(box_id) {//Funkcja sprawdza czy przycisk typu checkbox
-//o identyfikatorze box_id jest zaznaczony
-//----------------------------------------
+function sprawdz_box(box_id) {
     var obiekt = document.getElementById(box_id);
     return obiekt.checked;
 }
 
-function sprawdz() { //Funkcja realizujaca sprawdzanie całego fomularza
-//wykorzystując funkcje pomocnicze
-//--------------------------------
-    var ok = true; //zmienna informująca o poprawnym wypełnieniu formularza
-//Definicje odpowiednich wyrażeń regularnych dla sprawdzenia
-//poprawności danych wprowadzonych do pól tekstowych
-    obiektNazw = /^[a-zA-Z]{2,20}$/; //wyrażenie regularne dla nazwiska
-    obiektemail =
-        /^([a-zA-Z0-9])+([.a-zA-Z0-9_-])*@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-]+)+/;
-    obiektWiek = /^[1-9][0-9]{1,2}$/;
-//Sprawdzanie kolejnych pól formularza.
-//w przypadku błędu - pojawia się odpowiedni komunikat
-    if (!sprawdzPole("nazw", obiektNazw)) {
+function sprawdz() {
+    var ok = true;
+
+    let obiektNazw = /^[a-zA-Z\s\-]{1,30}$/;
+    let obiektWiek = /^(1[6-9]|[2-9][0-9]|1[0-1][0-9]|120)$/;
+    let obiektemail = /^([a-zA-Z0-9])+([.a-zA-Z0-9_-])*@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-]+)+$/;
+
+    if (!sprawdzPole("surname", obiektNazw)) {
         ok = false;
-        document.getElementById("nazw_error").innerHTML =
-            "Wpisz poprawnie nazwisko!";
-    } else document.getElementById("nazw_error").innerHTML = "";
-//Uzupełnij – sprawdz kolejne pola formularza
-// if ...
+        document.getElementById("surname_error").innerHTML = "Wpisz poprawnie nazwisko!";
+    } else document.getElementById("surname_error").innerHTML = "";
+
+    if (!sprawdzPole("age", obiektWiek)) {
+        ok = false;
+        document.getElementById("age_error").innerHTML = "Wpisz poprawnie wiek!";
+    } else document.getElementById("age_error").innerHTML = "";
+
+    if (!sprawdzPole("email", obiektemail)) {
+        ok = false;
+        document.getElementById("mail_error").innerHTML = "Wpisz poprawnie mail!";
+    } else document.getElementById("mail_error").innerHTML = "";
+
+    if (!sprawdz_radio("payment_method")) {
+        ok = false;
+        document.getElementById("payment_error").innerHTML = "Wybierz metodę płatności!";
+    } else document.getElementById("payment_error").innerHTML = "";
+
+    if (!(sprawdz_box("PHP") || sprawdz_box("C/C++") || sprawdz_box("Java"))) {
+        ok = false;
+        document.getElementById("language_error").innerHTML = "Wybierz co najmniej jeden język!";
+    } else document.getElementById("language_error").innerHTML = "";
+
     return ok;
 }
